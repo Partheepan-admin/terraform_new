cb28201a63f0fd5d1139b96d684ed7e8e31b2745
Partheepan-admin <124561854+Partheepan-admin@users.noreply.github.com>
2023-08-02 14:31:46 +0530

    Update the code into main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 resource "aws_instance" "terraform-test" {
   ami = "ami-0ded8326293d3201b"
   instance_type = "t2.micro"
-  key_name = "EFS-testkey"
-  security_groups = ["launch-wizard-1"]
-
   tags = {
-     Name = "terra-import"
+     Name = "terraform"
    }
 }
