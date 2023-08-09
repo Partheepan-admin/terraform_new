87c740814a0b94c2cc93670945e9cdff70fb09b5
Partheepan-admin <124561854+Partheepan-admin@users.noreply.github.com>
2023-08-09 13:11:43 +0530

    Code updated in main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "terraform-test" {
   ami = "ami-0ded8326293d3201b"
   instance_type = "t2.micro"
   tags = {
-     Name = "terraform"
+     Name = "terraform-Jenkins"
    }
 }
