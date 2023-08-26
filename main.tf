34f2cde0daf0513f9177a26f8127279e5bc6845a
Partheepan-admin <partheepan620@gmail.com>
2023-08-26 20:28:48 +0530

    meta agruments has been updated

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "terraform-demo" {
    ami = "ami-06f621d90fa29f6d0"
+   count = 4
    instance_type = "t2.micro"
    key_name = "testkey"
    tags = {
-      Name = "terra-test"  
+      Name = "terra-test_${count.index}" 
       }
 }  
