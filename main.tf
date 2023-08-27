d62c4875df96c7787d0d3c74e26303563c2a319e
Partheepan-admin <partheepan620@gmail.com>
2023-08-27 12:59:05 +0530

    main.tf fle has been updated

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "terraform-demo" {
    ami = "ami-06f621d90fa29f6d0"
-   count = 4
    instance_type = "t2.micro"
    key_name = "testkey"
    tags = {
-      Name = "terra-test_${count.index}"
+      Name = "terra-test"
       }
 }  
