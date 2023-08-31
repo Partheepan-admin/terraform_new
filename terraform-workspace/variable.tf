ff503bd114c2d3d4abb5d62cdd905072e3a4accc
Partheepan-admin <partheepan620@gmail.com>
2023-08-31 11:26:18 +0530

    workspace directory created

---
+++ terraform-workspace/variable.tf
@@ -0,0 +1,14 @@
+variable "instance_name" {
+    type = string
+    default = "workspace-demo"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "instance_ami" {
+    type = string
+    default = "ami-0da59f1af71ea4ad2"
+} 
