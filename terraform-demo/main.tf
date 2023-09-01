70bdd00bddd8be2a8777e4f876ea4ab3c465324e
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 17:36:21 +0530

    code updated

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -1,8 +1,13 @@
-resource "aws_vpc" "demo-vpc" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "demo-vpc" {
   tags = {
     Name = "my-vpc"
   }
 }
 
-
+resource "aws_subnet "demo-subnet" {
+  vpc_id = data.aws_vpc.demo-vpc.id
+  cidr_block = "10.0.2.0/24"
+    tags = {
+        Name = "my-subnet"
+    }
+  }
