fbdafcac01939dbace72c4580afc04018a532d2e
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 17:18:24 +0530

    main.tf file has been updated

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -13,8 +13,10 @@ resource "aws_subnet" "demo-subnet" {
 resource "aws_instance" "demo-instance" {
   ami = "ami-06f621d90fa29f6d0"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.demo-subnet.id
+  
   tags = {
     Name = "my-instance"
   }
+  
+  depends_on = [aws_subnet.demo-subnet]
 }
