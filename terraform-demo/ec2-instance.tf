e8df3d41a1e2c869293763a8b7158e8b8270eab7
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 20:19:04 +0530

    lifecycle code has been updated

--- terraform-demo/ec2-instance.tf
+++ terraform-demo/ec2-instance.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "demo-lifecycle" {
   ami =  "ami-06f621d90fa29f6d0"
   instance_type = "t2.micro"
-  tags = {
-    Name = "lifecycle-insta"
-  }
+  
   lifecycle {
-    create_before_destroy = true
+    ignore_changes = [ 
+      tags , # Ignore changes to tags
+      instance_type , # Ignore changes to the instance type 
+     ]
   }
 }
