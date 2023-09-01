239c670a1caba570aa6c9e536ff156c605c9d0a2
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 21:10:33 +0530

    code has been updated

--- terraform-demo/ec2-instance.tf
+++ terraform-demo/ec2-instance.tf
@@ -5,18 +5,6 @@ resource "aws_instance" "demo-lifecycle" {
   tags = {
     Name = var.instance_name
   }
-  
-   connection {
-     type = "ssh"
-     user = "ec2-user"
-     host = self.public_ip
-     private_key = file("demo-terraform.pem")
-   }
-   provisioner "remote-exec" {
-    inline = [
-      "sudo yum install git -y"
-    ]
-   }
 }
 
 
