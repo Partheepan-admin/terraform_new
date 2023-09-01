fe5b1e818ca8a4f2166cabdde5d19a7c4991cc2a
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 18:50:11 +0530

    code have updated

--- terraform-demo/ec2-instance.tf
+++ terraform-demo/ec2-instance.tf
@@ -1,1 +1,10 @@
-
+resource "aws_instance" "demo-lifecycle" {
+  ami =  "ami-06f621d90fa29f6d0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "lifecycle-insta"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
