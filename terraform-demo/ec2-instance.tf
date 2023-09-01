d79a94f72642bb22abb969dac77fa997e6c84107
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 18:14:19 +0530

    Iam roles code updated

--- terraform-demo/ec2-instance.tf
+++ terraform-demo/ec2-instance.tf
@@ -1,9 +1,1 @@
-resource "aws_instance" "Iam-instance" {
-  ami           = "ami-06f621d90fa29f6d0"
-  instance_type = "t2.micro"
-  iam_instance_profile = aws_iam_role.demo_role.name
-  
-  tags = {
-    Name = "Iam-user"
-  }
-}
+
--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -42,3 +42,14 @@ resource "aws_iam_policy_attachment" "demo_attachment" {
   policy_arn = aws_iam_policy.demo_policy.arn
   roles      = [aws_iam_role.demo_role.name]
 }
+
+
+resource "aws_instance" "Iam-instance" {
+  ami           = "ami-06f621d90fa29f6d0"
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_role.demo_role.name
+  
+  tags = {
+    Name = "Iam-user"
+  }
+}
