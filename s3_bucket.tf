d43ae26a5bc56afb966833c5be7ac4ace0a3f818
Partheepan-admin <partheepan620@gmail.com>
2023-09-01 19:33:52 +0530

    lifecycle policies updated

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -3,4 +3,10 @@ resource "aws_s3_bucket" "demo-bucket" {
   tags = {
     Name  = "My-bucket"
   }
+  lifecycle {
+    ignore_changes = [ 
+      versioning , # ignore the changes in versoning
+      acl , # ignore the changes in access control list
+     ]
+  }
 }
