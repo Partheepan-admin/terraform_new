22796c2c339ccee6c6bab4624f6b7d1b42993cb9
Partheepan-admin <partheepan620@gmail.com>
2023-09-02 12:49:25 +0530

    workspace has been deleted

--- terraform-workspace/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "awsglobal-bucket"
-#     key            = "workspace-name/terraform.tfstate"
-#     region         = "ap-south-1"
-#     encrypt        = true
-   
-#   }
-# }
--- terraform-workspace/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "demo-workspace" {
-   ami = var.instance_ami
-   instance_type = var.instance_type
-   key_name = "testkey"
-   tags = {
-      Name = var.instance_name  
-      }
-}
-
