ac01e95589aa5e53a8665927ecfbee1ec7c2bf17
Partheepan-admin <partheepan620@gmail.com>
2023-09-02 12:32:01 +0530

    output.tf file updated with the output data

--- User-module/output.tf
+++ User-module/output.tf
@@ -3,12 +3,12 @@ output "user_arn" {
     value = aws_iam_user.demo-user.arn
 }
 
-output "ec2_instance_public_ip" {
+output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.module-insta.public_ip
 }
 
-output "ec2_instance_id" {
+output "instance_id" {
   description = "Instance ID of the EC2 instance"
   value       = aws_instance.module-insta.id
 }
