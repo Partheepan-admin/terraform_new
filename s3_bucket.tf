5fa0f97ef63ccec7b9be7897c3d82e3a33d5531b
Partheepan-admin <partheepan620@gmail.com>
2023-09-02 10:31:56 +0530

    module code have updated

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,12 +1,6 @@
-resource "aws_s3_bucket" "demo-bucket" {
-  bucket = "awsglobal-bucket"
-  tags = {
-    Name  = "My-bucket"
-  }
-  lifecycle {
-    ignore_changes = [ 
-      versioning , # ignore the changes in versoning
-      acl , # ignore the changes in access control list
-     ]
-  }
-}
+# resource "aws_s3_bucket" "demo-bucket" {
+#   bucket = "awsglobal-bucket"
+#   tags = {
+#     Name  = "My-bucket"
+#   }
+# }
