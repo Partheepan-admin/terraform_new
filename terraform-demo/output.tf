70e6993d8f9e28dc5a9fb84b76890140c5ae9cee
Partheepan-admin <partheepan620@gmail.com>
2023-09-02 21:05:06 +0530

    output file updated

--- terraform-demo/output.tf
+++ terraform-demo/output.tf
@@ -1,9 +1,9 @@
-# output "instance_public_ip" {
-#   description = "Public IP address of the EC2 instance"
-#   value       = aws_instance.demo-foreach.public_ip
-# }
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.demo-foreach[each.key]
+}
 
-# output "instance_id" {
-#   description = "Instance ID of the EC2 instance"
-#   value       = aws_instance.demo-foreach.id
-# }
+output "instance_id" {
+  description = "Instance ID of the EC2 instance"
+  value       = aws_instance.demo-foreach[each.key]
+}
