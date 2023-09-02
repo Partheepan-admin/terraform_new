df25d43c12aa0bb28f268d1fde50b00cc1b45fbe
Partheepan-admin <partheepan620@gmail.com>
2023-09-02 12:56:18 +0530

    terraform-workspace folder updated

---
+++ terraform-workspace/variable.tf
@@ -0,0 +1,14 @@
+variable "instance_ami" {
+  type = string
+  default = "ami-06f621d90fa29f6d0"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  type = string
+  default = "workspace"
+}
