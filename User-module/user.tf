2a09e8366d7ef77e100b27f8c17af6351a3b0131
Partheepan-admin <partheepan620@gmail.com>
2023-09-05 21:18:46 +0530

    module file updated

--- User-module/user.tf
+++ User-module/user.tf
@@ -1,11 +1,3 @@
-resource "aws_iam_user" "demo-user" {
+resource "aws_iam_user" "demo-iam-user" {
   name = var.user_name
-}
-
-resource "aws_instance" "module-insta" {
-    ami = var.instance_ami
-    instance_type = var.instance_type
-    tags = {
-      Name = var.instance_name
-    }
 }
--- User-module/variable.tf
+++ User-module/variable.tf
@@ -1,20 +1,20 @@
 variable "user_name" {
   description = "Name of the Iam User"
   type = string
-  default = "my-Iam-user"
+  default = "AWS-iam-user"
 }
 
-variable "instance_ami" {
-  type = string
-  default = "ami-06f621d90fa29f6d0"
-}
+# variable "instance_ami" {
+#   type = string
+#   default = "ami-06f621d90fa29f6d0"
+# }
 
-variable "instance_type" {
-  type = string
-  default = "t2.micro"
-}
+# variable "instance_type" {
+#   type = string
+#   default = "t2.micro"
+# }
 
-variable "instance_name" {
-  type = string
-  default = "Module-Instance"
-}
+# variable "instance_name" {
+#   type = string
+#   default = "Module-Instance"
+# }
