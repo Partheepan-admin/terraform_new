82df7827afc76d974d61b5d930f0e81d75be5528
Partheepan-admin <partheepan620@gmail.com>
2023-09-06 22:54:02 +0530

    module code updated

--- User-module/user.tf
+++ User-module/user.tf
@@ -6,7 +6,7 @@ resource "aws_iam_policy_attachment" "demo-attachment" {
   name = "AWS-policy-attachment"
   policy_arn = aws_iam_policy.demo-policy.arn
   users = [aws_iam_user.demo-iam-user.name]
-  roles = aws_iam_role.demo-iam-role.name
+  roles = [aws_iam_role.demo-iam-role.name]
 }
 
 
